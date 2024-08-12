1b1cc8668f846d620889def1709e471e28b5816b
byrchard <batuhanrecep06@hotmail.com>
2024-08-12 21:25:34 +0300

    For testing CQRS BrandsController and BaseController coded. Application Layer's Service Registration implemantation added to program cs

---
+++ rentACar/RentACar/WebApi/Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+public class BaseController : ControllerBase
+{
+    private IMediator? _mediator;
+    protected IMediator? Mediator => _mediator??= HttpContext.RequestServices.GetService<IMediator>(); //if there is a mediator return that. If there isn't, do this (mediator null or not null)
+}
