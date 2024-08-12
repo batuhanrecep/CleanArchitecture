554cc7a0177b02ae56eb2e4424844553fe6471e4
byrchard <batuhanrecep06@hotmail.com>
2024-08-12 19:37:50 +0300

    For our Entities base class created

--- corePackages/Core.Packages/Core.Persistence/Repositories/Entity.cs
+++ corePackages/Core.Packages/Core.Persistence/Repositories/Entity.cs
@@ -6,9 +6,22 @@ using System.Threading.Tasks;
 
 namespace Core.Persistence.Repositories;
 
-public class Entity
+public class Entity<TId>
 {
-    //deneme
-    //b
+    public TId Id { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; } //? means nullable here
+    public DateTime? DeletedDate { get; set; }
+
+    public Entity()
+    {
+        Id = default;
+    }
+
+    public Entity(TId id)
+    {
+        Id = id;
+    }
 }
 
