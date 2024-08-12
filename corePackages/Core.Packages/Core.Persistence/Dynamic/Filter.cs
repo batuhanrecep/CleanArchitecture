3f1ebb46591a69cc63837ffcfe3667fd7baa78db
byrchard <batuhanrecep06@hotmail.com>
2024-08-12 23:42:42 +0300

    Filter and Sort system added

---
+++ corePackages/Core.Packages/Core.Persistence/Dynamic/Filter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Persistence.Dynamic;
+public class Filter
+{
+    public string Field { get; set; }
+    public string? Value { get; set; }
+    public string Operator { get; set; }
+    public string? Logic { get; set; }
+
+
+    public IEnumerable<Filter> filters { get; set; }
+
+    public Filter()
+    {
+        Field = string.Empty;
+        Operator = string.Empty;
+    }
+
+    public Filter(string field, string @operator) //There is an operator type int C#, to not get that we are adding @ 
+    {
+        Field = field;
+        Operator = @operator;
+    }
+}
