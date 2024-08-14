af5c1a13fc21abbdf5466cb8c685946320d1f4c3
byrchard <batuhanrecep06@hotmail.com>
2024-08-14 22:21:22 +0300

    For getlist operation our controller updated

--- rentACar/RentACar/WebApi/Controllers/BrandsController.cs
+++ rentACar/RentACar/WebApi/Controllers/BrandsController.cs
@@ -1,4 +1,7 @@
 ﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Queries.GetList;
+using Core.Application.Requests;
+using Core.Application.Responses;
 using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -16,5 +19,15 @@ namespace WebApi.Controllers
             CreatedBrandResponse response = await Mediator.Send(createBrandCommand);
             return Ok(response);
         }
+
+
+        [HttpGet]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            GetListBrandQuery getListBrandQuery = new() { PageRequest = pageRequest };
+            GetListResponse<GetListBrandListItemDto> response = await Mediator.Send(getListBrandQuery);
+            return Ok(response);
+        }
+
     }
 }
