e68211ff33bf9c158d9f29f6eecb0c71abed0cc1
byrchard <batuhanrecep06@hotmail.com>
2024-08-16 20:26:50 +0300

    Cache system implemented

--- rentACar/RentACar/Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
+++ rentACar/RentACar/Application/Features/Brands/Queries/GetList/GetListBrandQuery.cs
@@ -1,5 +1,6 @@
 ﻿using Application.Services.Repositories;
 using AutoMapper;
+using Core.Application.Pipelines.Caching;
 using Core.Application.Requests;
 using Core.Application.Responses;
 using Core.Persistence.Paging;
@@ -12,9 +13,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Application.Features.Brands.Queries.GetList;
-public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDto>>
+public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDto>>, ICachableRequest
 {
     public PageRequest PageRequest { get; set; }
+
+    //For Caching 
+    public string CacheKey => $"GetListBrandQuery({PageRequest.PageIndex},{PageRequest.PageSize})"; //Page Based
+    public bool BypassCache { get; }
+    public TimeSpan? SlidingExpiration { get; }
+
 
     public class GetListBrandQueryHandler : IRequestHandler<GetListBrandQuery, GetListResponse<GetListBrandListItemDto>>
     {
@@ -40,4 +47,6 @@ public class GetListBrandQuery : IRequest<GetListResponse<GetListBrandListItemDt
             return response;
         }
     }
+
+
 }
