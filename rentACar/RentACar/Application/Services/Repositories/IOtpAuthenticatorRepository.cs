82cf0f131189d9602316573bfc065503086f029c
byrchard <batuhanrecep06@hotmail.com>
2024-08-18 23:34:12 +0300

    In application layer, user and security related repository interfaces written

---
+++ rentACar/RentACar/Application/Services/Repositories/IOtpAuthenticatorRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+
+namespace Application.Services.Repositories;
+
+public interface IOtpAuthenticatorRepository : IAsyncRepository<OtpAuthenticator, int>,
+    IRepository<OtpAuthenticator, int>
+{
+
+}
---
+++ rentACar/RentACar/Application/Services/Repositories/IUserOperationClaimRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.Persistence.Repositories;
+using Core.Security.Entities;
+
+namespace Application.Services.Repositories;
+
+public interface IUserOperationClaimRepository : IAsyncRepository<UserOperationClaim, int>, IRepository<UserOperationClaim, int>
+{
+    Task<IList<OperationClaim>> GetOperationClaimsByUserIdAsync(int userId);
+}
