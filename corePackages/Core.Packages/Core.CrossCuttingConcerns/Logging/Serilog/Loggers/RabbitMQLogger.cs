a8548687c965bb04925e8020cc32121d2d3a594e
byrchard <batuhanrecep06@hotmail.com>
2024-08-23 07:19:34 +0300

    RabbitMQLogger written

---
+++ corePackages/Core.Packages/Core.CrossCuttingConcerns/Logging/Serilog/Loggers/RabbitMQLogger.cs
@@ -0,0 +1,44 @@
+﻿using Core.CrossCuttingConcerns.Logging.Serilog.ConfigurationModels;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.RabbitMQ;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.CrossCuttingConcerns.Logging.Serilog.Loggers;
+public class RabbitMQLogger : LoggerServiceBase
+{
+    public RabbitMQLogger(IConfiguration configuration)
+    {
+        RabbitMQConfiguration rabbitMQConfiguration = configuration
+            .GetSection("SeriLogConfigurations:RabbitMQConfiguration")
+            .Get<RabbitMQConfiguration>();
+
+        RabbitMQClientConfiguration config =
+            new()
+            {
+                Port = rabbitMQConfiguration.Port,
+                DeliveryMode = RabbitMQDeliveryMode.Durable,
+                Exchange = rabbitMQConfiguration.Exchange,
+                Username = rabbitMQConfiguration.Username,
+                Password = rabbitMQConfiguration.Password,
+                ExchangeType = rabbitMQConfiguration.ExchangeType,
+                RouteKey = rabbitMQConfiguration.RouteKey
+            };
+        rabbitMQConfiguration.Hostnames.ForEach(x => config.Hostnames.Add(x));
+
+        Logger = new LoggerConfiguration().WriteTo
+            .RabbitMQ(
+                (clientConfiguration, sinkConfiguration) =>
+                {
+                    clientConfiguration.From(config);
+                    sinkConfiguration.TextFormatter = new JsonFormatter();
+                }
+            )
+            .CreateLogger();
+    }
+}
